b332b60d4f6e74525222f4133e382c7ef3eb18eb
borawskiD <borawskid@icloud.com>
2022-11-10 21:16:26 +0100

    Refactor of whole algorithm

--- src/com/company/Task.java
+++ src/com/company/Task.java
@@ -1,114 +1,84 @@
 package com.company;
 
 public class Task {
-    double v0 = 1;
-    int k = 1;
-    int N = 2;
-    double epsilon = 0.0001;
-    double[] P0 = new double[]{1, 1}; //punt startowy
-    double[] Pz = new double[]{1, 1}; //punt zastepczy, do porownania
-    double[] Pp = new double[]{1, 1}; //punt poprzedni, do odejmowania
+    double previousPoint[] = new double[]{1,1};
+    double valueOfPreviousPoint = F(previousPoint[0], previousPoint[1]);
+    double currentPoint[] = new double[]{1,1};
+    double valueOfCurrentPoint = F(currentPoint[0], currentPoint[1]);
+    boolean timeToStop = false;
+    final double EPSILON = 0.0001;
+    double v = 1;
+    public void run(){
+        do{
+            findNewPoint();
+            moveForward();
+            if (v < EPSILON) timeToStop = true;
+            else{
+                //System.out.println("Wartosc kroku przekracza dokladnosc (epsilon). Algorytm konczy dzialanie");
+                System.out.println("Krok: " + v);
+                System.out.println("Epsilon: " + EPSILON);
+            }
 
-    double F0 = f(P0);
-    boolean secondTested = false;
-    boolean nothingChanged = true;
-
-    public void run() {
-        do {
-            prepareBeforeTaskOne();
-            System.out.println("******");
-            System.out.println("Task 2");
-            System.out.println("******");
-            taskTwo();
-            System.out.println("Zakonczylem poszukiwania:");
-            System.out.println("Biezacy punkt: [" + Pz[0] + ", " + Pz[1] + "]; f(x) wynosi: " + f(Pz));
-            System.out.println();
-        } while (!nothingChanged);
-
+        }while(!timeToStop);
     }
 
-    void prepareBeforeTaskOne() {
-        nothingChanged = true;
-        secondTested = false;
-        k = 1;
-        System.out.println("******");
-        System.out.println("Task 1");
-        System.out.println("******");
-        taskOne();
+    private void moveForward() {
+        valueOfCurrentPoint = F(currentPoint[0], currentPoint[1]);
+        if (valueOfCurrentPoint < valueOfPreviousPoint){
+            double x1 = previousPoint[0] + 1.5 * (currentPoint[0] - previousPoint[0]);
+            double x2 = previousPoint[1] + 1.5 * (currentPoint[1] - previousPoint[1]);
+            System.out.println("Punkt po przesunieciu: [" + x1 + ", " + x2 + "]");
+            previousPoint = currentPoint;
+            valueOfPreviousPoint = valueOfCurrentPoint;
+            currentPoint = new double[]{x1, x2};
+        }
+        else{
+            v = 0.2 * v;
+        }
+    }
+
+    public void findNewPoint(){
+        System.out.println("");
+        System.out.println("======");
+        System.out.println("Wartosc kroku: " + v);
+        //first side
+        System.out.println("Analiza punktu - 1 wymiar:");
+        double[] newValue = checkPoints(currentPoint[0] + v, currentPoint[1]);
+        System.out.println("Ostateczny punkt po sprawdzeniu 1. wymiaru: [" + newValue[0] + ",  " + newValue[1]+"] gdzie f(x) = " + F(newValue[0], newValue[1]));
+        //second side
         System.out.println("---");
-        System.out.println("Ostatecznie wybrany punkt: [" + P0[0] + "; " + P0[1] + "] y = " + F0 + " a krok: " + v0);
+        System.out.println("Analiza punktu - 2 wymiar:");
+        newValue = checkPoints(currentPoint[0], currentPoint[1] + v);
+        System.out.println("Ostateczny punkt po sprawdzeniu 2. wymiaru: [" + newValue[0] + ",  " + newValue[1]+"] gdzie f(x) = " + F(newValue[0], newValue[1]));
+        if (newValue == currentPoint) v = 1.5 * v;
+        else{
+            System.out.println("Znaleziony punkt okazuje sie najlepszym - koniec dzialania programu");
+            timeToStop = true;
+        }
     }
-
-    void taskOne() {
-        boolean foundBetter = false;
-        double[] Pr = new double[]{1, 1};
-        if (k == 1) Pr = new double[]{Pz[0] + v0, Pz[1]}; //P roboczy
-        if (k == 2) Pr = new double[]{Pz[0], Pz[1] + v0}; //P roboczy dla 2 wymiaru
-        double Fr = f(Pr);
-        System.out.println("Aktualnie analizuje punkt dla " + k + " zmiennej: [" + Pr[0] + "; " + Pr[1] + "] dla ktorej f(x) wynosi: " + Fr + " a krok: " + v0);
-        if (Fr < F0) {
-            System.out.println("Sukces - ten punkt staje sie nowym punktem roboczym.");
-            P0 = Pr;
-            F0 = Fr;
-            foundBetter = true;
-            nothingChanged = false;
+    double[] checkPoints(double x1, double x2){
+        //doing full analysis for 1d
+        double[] newPoint = new double[]{x1, x2};
+        double fNP = F(newPoint[0], newPoint[1]);
+        System.out.println("Aktualnie analizuje punkt: [" + newPoint[0] + ", " + newPoint[1] + "]" + " gdzie f(x) = " + fNP);
+        if (fNP < valueOfCurrentPoint){
+            currentPoint = newPoint;
+            System.out.println("Jest lepszy od poprzednika, zatem staje sie najlepszym punktem.");
         }else{
-            System.out.println("Nie wybieramy tego punktu (poprzednik jest lepszy).");
+            System.out.println("Nie jest lepszy od poprzednika, zatem analizuje drugi punkt:");
+            newPoint[0] = newPoint[0] - 2 * v;
+            fNP = F(newPoint[0], newPoint[1]);
+            System.out.println("Aktualnie analizuje punkt: [" + newPoint[0] + ", " + newPoint[1] + "]" + " gdzie f(x) = " + fNP);
+            if (fNP < valueOfCurrentPoint){
+                currentPoint = newPoint;
+                System.out.println("Jest lepszy od poprzednikow, zatem staje sie najlepszym punktem");
+            }else{
+                System.out.println("Nie lepszy od poprzednikow, nic nie zmieniam");
+            }
         }
-        if (secondTested && !foundBetter) v0 = -1 * v0;
-        if (!secondTested) {
-            System.out.println("Sprawdzam przeciwny kierunek:");
-            v0 = -1 * v0;
-            secondTested = true;
-            taskOne();
-        }
-        Pz = P0;
-        if (k < N) {
-            System.out.println();
-            System.out.println("Sprawdzam druga zmienna");
-            secondTested = false;
-            k++;
-            taskOne();
-        }
+        return newPoint;
     }
-
-    void taskTwo() {
-        double x1Diff = Pp[0] + 1.5 * (Pz[0] - Pp[0]);
-        double x2Diff = Pp[1] + 1.5 * (Pz[1] - Pp[1]);
-        double newValue = f(new double[]{x1Diff, x2Diff});
-        System.out.println("Sprawdzam punkt: [" + x1Diff + "; " + x2Diff + "] dla ktorego f(x) wynosi: " + newValue);
-        if (Math.abs((newValue - F0)) == 0) taskFour();
-        if (newValue < F0 && (Math.abs((newValue - F0)) > epsilon)) {
-            System.out.println("Ten punkt spelnia kryteria - jest mniejszy od poprzednika oraz roznica miesci sie w oczekiwanym przedziale.");
-            System.out.println();
-            Pp = Pz;
-            Pz = new double[]{x1Diff, x2Diff};
-            F0 = f(Pz);
-            taskTwo();
-        }
-    }
-
-    void taskFour() {
-        if (v0 > epsilon) {
-            System.out.println();
-            System.out.println("Zmieniam wartosc kroku: ");
-            System.out.println("Epsilon: " + epsilon);
-            System.out.println("Stara wartosc kroku: " + v0);
-            v0 = 0.1 * v0;
-            System.out.println("Nowa wartosc kroku: " + v0);
-            System.out.println();
-            nothingChanged = false;
-        } else {
-            System.out.println("**********************************************************************");
-            System.out.println("Przekroczono oczekiwana dokladnosc (" + epsilon + "). Algorytm konczy dzialanie");
-            System.out.println();
-            nothingChanged = true;
-        }
-    }
-
-    double f(double[] args) {
-        double x1 = args[0];
-        double x2 = args[1];
-        return -x1 * x1 - x1 * x1 * x2 * x2 + 3 * x1 * x2;
+    double F(double x1, double x2){
+        return  -x1 * x1 - x1 * x1 * x2 * x2 + 3 * x1 * x2;
     }
 }
